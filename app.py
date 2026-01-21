80e1e8a7122f2aeb83b2046e47d09ebbef4d9b49
NguyenThanhTien539 <161334882+NguyenThanhTien539@users.noreply.github.com>
2026-01-21 21:33:04 +0700

    init project

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/test')
+def hello():
+    return {'message': 'Hello World!'}
+
+
+# Addition operation
+@app.route('/api/add', methods=['POST'])
+def add():
+    data_request = request.get_json()
+    if (not data_request or 'number_1' not in data_request or
+            'number_2' not in data_request):
+        return jsonify({'error': 'Invalid input'}), 400
+
+    number_1 = float(data_request['number_1'])
+    number_2 = float(data_request['number_2'])
+    result = number_1 + number_2
+    return jsonify({'result': result})
+
+
+# Optional: Completing the following TODOs is optional for more practice
+
+# TODO: Implement the 'multiplication operation'
+@app.route('/api/multiply', methods=['POST'])
+def multiply():
+    data_request = request.get_json()
+    if (not data_request or 'number_1' not in data_request or
+            'number_2' not in data_request):
+        return jsonify({'error': 'Invalid input'}), 400
+    number_1 = float(data_request['number_1'])
+    number_2 = float(data_request['number_2'])
+    result = number_1 * number_2
+    return jsonify({'result': result})
+
+# TODO: Implement the 'subtraction operation'
+@app.route('/api/subtract', methods=['POST'])
+def subtract():
+    # Write code here
+    data_request = request.get_json()
+    if (not data_request or 'number_1' not in data_request or
+            'number_2' not in data_request):
+        return jsonify({'error': 'Invalid input'}), 400
+    number_1 = float(data_request['number_1'])
+    number_2 = float(data_request['number_2'])  
+    result = number_1 - number_2
+    return jsonify({'result': result})
+
+# TODO: Implement the 'division operation'
+@app.route('/api/divide', methods=['POST'])
+def divide():
+    # Write code here
+    data_request = request.get_json()
+    if (not data_request or 'number_1' not in data_request or
+            'number_2' not in data_request):
+        return jsonify({'error': 'Invalid input'}), 400
+    number_1 = float(data_request['number_1'])
+    number_2 = float(data_request['number_2'])
+    if number_2 == 0:
+        return jsonify({'error': 'Division by zero'}), 400
+    result = number_1 / number_2
+    return jsonify({'result': result})
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
